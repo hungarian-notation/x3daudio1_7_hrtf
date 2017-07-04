66b2a67a012538eace4864a19d1e533aa532d578
Roman Zavalov <japangod@yandex.ru>
2017-07-04 04:21:35 +0300

    Added automated build script which outputs final packages for distribution

---
+++ build-script/build.bat
@@ -0,0 +1,25 @@
+:tools_paths
+@set "VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+@set "SEVENZ_DIR=C:\Program Files\7-Zip"
+
+@set "PATH=%VSINSTALLDIR%\MSBuild\15.0\bin;%PATH%"
+@set "PATH=%SEVENZ_DIR%;%PATH%"
+
+
+:set_variables
+@set "VERSION=2.0"
+@set "PRODUCT_NAME=xaudio_hrtf"
+
+
+:set_subpaths
+@set "INTERIM_DIR_NAME=interim"
+@set "INTERIM_DIR=..\%INTERIM_DIR_NAME%"
+@set "COMPILATION_DIR=%INTERIM_DIR%\compilation"
+@set "UNCOMPRESSED_PACKAGE_DIR=%INTERIM_DIR%\package"
+@set "FINAL_PACKAGE_DIR=..\final_package"
+@set "HRTF_DATASET_DIR=..\hrtf\KEMAR"
+
+call build_platform Win32
+call build_platform x64
+
+echo Build Complete
